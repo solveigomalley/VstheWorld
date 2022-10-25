74f1bab5eef8fb747bb144a9a0a1ced9933fd4af
solveigomalley <116644561+solveigomalley@users.noreply.github.com>
2022-10-25 13:49:47 +0200

    Update VstheWorld.js
    Modifications to logging
    Added set user agent
    Changed wait to #button-join from .audience

--- VstheWorld.js
+++ VstheWorld.js
@@ -25,9 +25,11 @@ const url = "https://jackbox.tv";
 process.setMaxListeners(Infinity);
 
 async function run(browser, sessionId) {
-    console.log('starting');
+    console.log('starting session ' + sessionId);
     const page = await browser.newPage();
-    console.log('got page');
+    //console.log('got page');
+    //set user agent
+    await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4182.0 Safari/537.36");
     // Configure the navigation timeout
     await page.setDefaultNavigationTimeout(0);
     //turns request interceptor on
@@ -41,34 +43,38 @@ async function run(browser, sessionId) {
             request.continue();
     });
 
-    console.log('about to go to jackbox');
+    //console.log('about to go to jackbox');
     // go to jackbox.tv
     await page.goto(url);
-    console.log('got jackbox page');
+    //console.log('got jackbox page');
 
     // enter room code
     await page.waitForSelector("#roomcode", { timeout: timeoutMilliseconds });
     await page.type("#roomcode", roomCode);
-    console.log('entered room code');
+    //console.log('entered room code');
 
     // enter user name
     await page.waitForSelector("#username", { timeout: timeoutMilliseconds });
     await page.type("#username", "User" + sessionId);
-    console.log('entered username ' + sessionId);
+    //console.log('entered username ' + sessionId);
 
     // click "Join Audience" button once available
-    await page.waitForSelector(".audience", { timeout: timeoutMilliseconds });
+    //await page.waitForSelector(".audience", { timeout: timeoutMilliseconds });
+    await page.waitForSelector("#button-join", { timeout: timeoutMilliseconds });
     await page.click("#button-join");
     console.log('joined audience');
+    console.log('joined audience with user' + sessionId);
 
     // When game ends, browser displays "DISCONNECTED", so we are done
     await page.waitForXPath('//*[contains(text(), "DISCONNECTED")]', { timeout: timeoutMilliseconds });
     console.log('disconnected');
-
     browser.close();
 }
 async function runAll() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    console.log('starting up sessions')
     // set up the sessions
     const sessions = [...Array(numberOfSessions).keys()].map(x => run(browser, x));
     // run all the sessions in parallel
